e524cfd7440b2ae1ad996cc30848966190025607
Chow Jia Ying <chowjiaying211@gmail.com>
2023-11-19 09:16:00 +0000

    feat: implement rest of the init command

--- app/adder_app.cpp
+++ app/adder_app.cpp
@@ -1,14 +1,20 @@
 #include "adder/adder.hpp"
 #include "commands/init.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main(int argc, char** argv){
   if (argc < 2) {
     std::cerr << "Usage: git [command]\n";
   }
   std::string command = argv[1];
+  std::vector<std::string> args;
+  for (int i = 0; i < argc; i++) {
+    args.push_back(argv[i]);
+  }
   if (command == "init") {
-    init(argv);
+    init(args);
   }
   return 0;
 }
--- src/init.cpp
+++ src/init.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "tclap/CmdLine.h"
 #include "commands/init.h"
-void init(char** argv) {
-    std::cout << "init";
+void init(std::vector<std::string> &args) {
+    TCLAP::CmdLine cmd("init", ' ', "0.9");
+
+    // defines arguments
+    TCLAP::ValueArg<std::string> path("p","path","Path to create the git repo",false,".","string");
+    cmd.ignoreUnmatched(true);
+    cmd.add(path);
+    cmd.parse(args);
+    std::string pathValue = path.getValue();
+    std::cout << "Path: " << pathValue << "\n";
 }
