78b0e8365dfd3b00c8d286dbf9ec6d981b3b68e5
Chow Jia Ying <chowjiaying211@gmail.com>
2025-04-08 22:21:39 +0800

    Implement gpgsig and remove newline character that is saved into the message

--- src/commit.cpp
+++ src/commit.cpp
@@ -20,6 +20,7 @@ std::string GitCommit::serialise(GitRepository &repo) {
      << this->keyValuePairs["committer_email"] << "> "
      << this->keyValuePairs["committer_unix_timestamp"] << " "
      << this->keyValuePairs["committer_timezone"] << "\n";
+  ss << "gpgsig " << this->keyValuePairs["gpgsig"] << "\n\n";
   ss << this->keyValuePairs["message"];
   return ss.str();
 }
@@ -28,12 +29,10 @@ std::string GitCommit::print_commit(GitRepository &repo) {
   std::stringstream ss;
   ss << "commit " << sha << "\n";
   ss << "Author: " << this->keyValuePairs["author_name"] << " <"
-     << this->keyValuePairs["author_email"] << "> "
-     << this->keyValuePairs["author_unix_timestamp"] << " "
-     << this->keyValuePairs["author_timezone"] << "\n";
+     << this->keyValuePairs["author_email"] << ">\n";
   ss << "Date: " << this->keyValuePairs["author_unix_timestamp"] << " "
-     << this->keyValuePairs["author_timezone"] << "\n";
-  ss << this->keyValuePairs["message"] << "\n";
+     << this->keyValuePairs["author_timezone"] << "\n\n";
+  ss << "    " << this->keyValuePairs["message"];
   return ss.str();
 }
 
@@ -41,8 +40,8 @@ void GitCommit::deserialise(const std::string &data) {
   const std::unordered_map<std::string, std::string> keyValuePairs;
   int pos = 0;
 
-  const std::array<std::string, 4> keys = {"tree", "parent", "author",
-                                           "committer"};
+  const std::array<std::string, 5> keys = {"tree", "parent", "author",
+                                           "committer", "gpgsig"};
   const std::vector<std::pair<std::string, std::string>> sub_keys = {
       {"name", " <"},
       {"email", "> "},
@@ -62,14 +61,21 @@ void GitCommit::deserialise(const std::string &data) {
             data.substr(pos, space - pos);
         pos = space + sub_key.second.size();
       }
+      pos = nl + 1;
+    } else if (key == "gpgsig") {
+      std::string gpgsig_end = "-----END PGP SIGNATURE-----";
+      nl = data.find(gpgsig_end, space) + gpgsig_end.size();
+      this->keyValuePairs[key] =
+          data.substr(space + key.size() + 1, nl - space - key.size() - 1);
+      pos = nl + 3;
     } else {
       this->keyValuePairs[key] =
           data.substr(space + key.size() + 1, nl - space - key.size() - 1);
+      pos = nl + 1;
     }
-    pos = nl + 1;
   }
   int nl = data.find("\n", pos + 1);
-  this->keyValuePairs["message"] = data.substr(pos, nl - pos);
+  this->keyValuePairs["message"] = data.substr(pos + 1, nl - pos);
 }
 
 bool GitCommit::has_parent() {
