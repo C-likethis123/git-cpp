687eca5c4182f9154fba39941a2cc57a2da88efd
Chow Jia Ying <chowjiaying211@gmail.com>
2025-05-14 22:55:26 +0800

    Write tests for tag command

--- tests/tag_t.cpp
+++ tests/tag_t.cpp
@@ -23,49 +23,56 @@ std::string file_contents(const fs::path &path) {
 const std::string FIRST_COMMIT = "1723ac93b92db1fc2c28de8e5da814136937f8c6";
 const std::string SECOND_COMMIT = "6c2c22e7b5b7b1682e3c14668499e84141aca0d4";
 const fs::path VALID_GIT_PATH = fs::temp_directory_path() / "gitrepo";
-const fs::path TEST_FILE_PATH = fs::current_path() / "gitrepo";
+const fs::path OLD_CWD = fs::current_path();
+const fs::path TEST_FILE_PATH = OLD_CWD / "gitrepo";
 
-void setup() {
-  // Provide a path to a valid git repository, set up a git repo with an
-  // actual commit
-  fs::copy(TEST_FILE_PATH, VALID_GIT_PATH, fs::copy_options::recursive);
+/**
+Uses RAII to manage the setup and teardown of a sample Git repo
+*/
+class GitRepoSetup {
+public:
+  GitRepoSetup() { setup(); }
+  ~GitRepoSetup() { teardown(); }
 
-  fs::current_path(VALID_GIT_PATH);
-}
+  void setup() {
+    // set up a git repo with an actual commit
+    fs::copy(TEST_FILE_PATH, VALID_GIT_PATH, fs::copy_options::recursive);
 
-void teardown() { fs::remove_all(VALID_GIT_PATH); }
+    fs::current_path(VALID_GIT_PATH);
+  }
 
-// TODO: test cases
+  void teardown() {
+    fs::remove_all(VALID_GIT_PATH);
+    fs::current_path(OLD_CWD);
+  }
+};
+
 TEST_CASE("tag command", "[tag]") {
-  setup();
-  SECTION("Valid git tag command - tag name only") {
-    std::vector<std::string> args({"tag", "v1.0"});
-    commands::tag(args);
+  GitRepoSetup gitRepoSetup;
+  SECTION("Valid git tag command - tag name only", "tag name only") {
+    {
+      std::vector<std::string> args({"tag", "v1.0"});
+      commands::tag(args);
 
-    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
-    REQUIRE(fs::exists(".git/refs/tags/v1.0"));
-    REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
+      REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+      REQUIRE(fs::exists(".git/refs/tags/v1.0"));
+      REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
+    }
+    SECTION("Valid git tag command - tag name with commit",
+            "tag name with commit") {
+      std::vector<std::string> args({"tag", "v1.0", SECOND_COMMIT});
+      commands::tag(args);
+
+      REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+      REQUIRE(fs::exists(".git/refs/tags/v1.0"));
+      REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
+    }
   }
-  teardown();
-
-  setup();
-  SECTION("Valid git tag command - tag name with commit") {
-    std::vector<std::string> args({"tag", "v1.0", SECOND_COMMIT});
-    commands::tag(args);
-
-    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
-    REQUIRE(fs::exists(".git/refs/tags/v1.0"));
-    REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
-  }
-  teardown();
 }
 
-TEST_CASE("tag with errors", "[tag]") {
-  setup();
-  SECTION("Git tag failed with missing tag name") {}
-  teardown();
-
-  setup();
-  SECTION("Git tag failed with non-existent commit") {}
-  teardown();
-}
+// TEST_CASE("tag with errors", "[tag errors]") {
+//   //   setup();
+//   SECTION("Git tag failed with missing tag name") {}
+//   SECTION("Git tag failed with non-existent commit") {}
+//   //   teardown();
+// }
