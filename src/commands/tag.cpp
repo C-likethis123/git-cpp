ee9e50bae0f49fdcdf36bd1c82768eddd96c9fe6
Chow Jia Ying <chowjiaying211@gmail.com>
2025-05-14 23:20:50 +0800

    Add test cases

--- src/commands/tag.cpp
+++ src/commands/tag.cpp
@@ -11,32 +11,28 @@ void tag(std::vector<std::string> &args) {
   TagParser &parser = TagParser::get();
   parser.parse(args);
   // process args
-  try {
-    std::optional<GitRepository> repo = GitRepository::find();
-    if (!repo) {
-      throw std::runtime_error("Not a git repository");
-    }
-    // resolve ref, basically find the commit object
-
-    std::string commit = parser.isCommitSet() ? parser.getCommit()
-                                              : GitObject::find(*repo, "HEAD");
-    std::string tag = parser.getTag();
-    // ERROR: if the commit doesn't exist.
-    if (fs::is_regular_file(get_commit_path(commit))) {
-      std::string error_message = commit + ": not a valid commit";
-      throw std::runtime_error(error_message);
-    }
-    // ERROR: if the tag already exists
-    fs::path tag_path = repo->repo_path("refs/tags/" + tag);
-    if (fs::is_regular_file(tag_path)) {
-      std::string error_message = "tag '" + tag + "' already exists";
-      throw std::runtime_error(error_message);
-    }
-
-    // create a new file in refs/tags/{name}, and write the sha of the commit.
-    create_file(tag_path, commit + "\n");
-  } catch (std::runtime_error &err) {
-    std::cerr << err.what() << "\n";
+  std::optional<GitRepository> repo = GitRepository::find();
+  if (!repo) {
+    throw std::runtime_error("Not a git repository");
   }
+  // resolve ref, basically find the commit object
+
+  std::string commit = parser.isCommitSet() ? parser.getCommit()
+                                            : GitObject::find(*repo, "HEAD");
+  std::string tag = parser.getTag();
+  // ERROR: if the commit doesn't exist.
+  if (fs::is_regular_file(get_commit_path(commit))) {
+    std::string error_message = commit + ": not a valid commit";
+    throw std::runtime_error(error_message);
+  }
+  // ERROR: if the tag already exists
+  fs::path tag_path = repo->repo_path("refs/tags/" + tag);
+  if (fs::is_regular_file(tag_path)) {
+    std::string error_message = "tag '" + tag + "' already exists";
+    throw std::runtime_error(error_message);
+  }
+
+  // create a new file in refs/tags/{name}, and write the sha of the commit.
+  create_file(tag_path, commit + "\n");
 }
 } // namespace commands
--- tests/tag_t.cpp
+++ tests/tag_t.cpp
@@ -50,29 +50,43 @@ public:
 TEST_CASE("tag command", "[tag]") {
   GitRepoSetup gitRepoSetup;
   SECTION("Valid git tag command - tag name only", "tag name only") {
-    {
-      std::vector<std::string> args({"tag", "v1.0"});
-      commands::tag(args);
+    std::vector<std::string> args({"tag", "v1.0"});
+    commands::tag(args);
 
-      REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
-      REQUIRE(fs::exists(".git/refs/tags/v1.0"));
-      REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
-    }
-    SECTION("Valid git tag command - tag name with commit",
-            "tag name with commit") {
-      std::vector<std::string> args({"tag", "v1.0", SECOND_COMMIT});
-      commands::tag(args);
+    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+    REQUIRE(fs::exists(".git/refs/tags/v1.0"));
+    REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
+  }
+  SECTION("Valid git tag command - tag name with commit",
+          "tag name with commit") {
+    std::vector<std::string> args({"tag", "v1.0", SECOND_COMMIT});
+    commands::tag(args);
 
-      REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
-      REQUIRE(fs::exists(".git/refs/tags/v1.0"));
-      REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
-    }
+    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+    REQUIRE(fs::exists(".git/refs/tags/v1.0"));
+    REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
   }
 }
 
-// TEST_CASE("tag with errors", "[tag errors]") {
-//   //   setup();
-//   SECTION("Git tag failed with missing tag name") {}
-//   SECTION("Git tag failed with non-existent commit") {}
-//   //   teardown();
-// }
+TEST_CASE("tag with errors", "[tag errors]") {
+  GitRepoSetup gitRepoSetup;
+  SECTION("Git tag already exists") {
+    std::vector<std::string> args({"tag", "v1.0"});
+    commands::tag(args);
+
+    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+    REQUIRE(fs::exists(".git/refs/tags/v1.0"));
+    REQUIRE(file_contents(".git/refs/tags/v1.0") == SECOND_COMMIT);
+
+    std::vector<std::string> args2({"tag", "v1.0"});
+    REQUIRE_THROWS_WITH(commands::tag(args2), "tag 'v1.0' already exists");
+  }
+  SECTION("Git tag failed with non-existent commit") {
+    std::vector<std::string> args({"tag", "v1.0", "nonexistentcommit"});
+    REQUIRE_NOTHROW(GitRepository(VALID_GIT_PATH, true));
+
+    REQUIRE_THROWS_WITH(commands::tag(args),
+                        "nonexistentcommit: not a valid commit");
+    REQUIRE(!fs::exists(".git/refs/tags/v1.0"));
+  }
+}
